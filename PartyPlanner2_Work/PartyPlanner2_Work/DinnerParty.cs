1f9717e8cd2f7aa3df225fe176953dc61add5031
David Baker <dbaker3@stevemail.com>
2013-04-16 12:18:47 -0400

    worked on new classes

---
+++ PartyPlanner2_Work/PartyPlanner2_Work/DinnerParty.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartyPlanner2_Work
+{
+    class DinnerParty
+    {
+        // Constructor
+        public DinnerParty(bool healthyOption, bool fancyDecorations, int numberOfPeople)
+        {
+            this.NumberOfPeople = numberOfPeople;
+            this.FancyDecorations = fancyDecorations;
+            this.HealthyOption = healthyOption;
+        }
+
+        const int CostOfFoodPerPerson = 25;
+
+        // Fields
+        private decimal costOfBeveragesPerPerson;
+        private decimal CostOfDecorations = 0;
+
+        // Backing fields
+        private int _numberOfPeople;
+        private bool _fancyDecorations;
+        private bool _healthyOption;
+
+        public int NumberOfPeople 
+        {
+            get { return _numberOfPeople; }
+            set
+            {
+                _numberOfPeople = value;
+                CalculateCostOfDecorations();
+            }
+        }
+
+        public bool FancyDecorations
+        {
+            get { return _fancyDecorations; }
+            set
+            {
+                _fancyDecorations = value;
+                CalculateCostOfDecorations();
+            }
+        }
+
+        public bool HealthyOption
+        {
+            set
+            {
+                if (value)
+                {
+                    costOfBeveragesPerPerson = 5.00M;
+                    _healthyOption = true;
+                }
+                else
+                {
+                    costOfBeveragesPerPerson = 20.00M;
+                    _healthyOption = false;
+                }
+            }
+            get
+            {
+                return _healthyOption;
+            }
+        }
+        
+        public void CalculateCostOfDecorations()
+        {
+            if (this.FancyDecorations) CostOfDecorations = (this.NumberOfPeople * 15.00M) + 50M;
+            else CostOfDecorations = (this.NumberOfPeople * 7.50M) + 30M;
+        }
+
+        public decimal CalculateCost()
+        {
+            decimal totalCost = CostOfDecorations + ((costOfBeveragesPerPerson + CostOfFoodPerPerson) * this.NumberOfPeople);
+
+            if (this.HealthyOption)
+                return totalCost * 0.95M;   // 5% discount applied
+            else
+                return totalCost;
+        }
+
+
+
+    }
+}
---
+++ PartyPlanner2_Work/PartyPlanner2_Work/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PartyPlanner2_Work
+{
+    public partial class Form1 : Form
+    {
+        DinnerParty dinnerParty;
+        BirthdayParty birthdayParty;
+                    
+        public Form1()
+        {
+            InitializeComponent();
+            dinnerParty = new DinnerParty(checkBoxDinnerHealthy.Checked, checkBoxDinnerFancy.Checked, (int)numericUpDownDinner.Value);
+            birthdayParty = new BirthdayParty(textBoxCakeWriting.Text);
+            DisplayDinnerPartyCost();
+            DisplayBirthdayPartyCost();
+        }
+
+        #region Birthday_Party
+
+        private void numericUpDownBirthday_ValueChanged(object sender, EventArgs e)
+        {
+            recalculateBirthdayEverything();
+            DisplayBirthdayPartyCost();
+        }
+
+        private void checkBoxBirthdayFancy_CheckedChanged(object sender, EventArgs e)
+        {
+            DisplayBirthdayPartyCost();
+        }
+
+        private void recalculateBirthdayEverything()
+        {
+            
+        }
+
+        private void DisplayBirthdayPartyCost()
+        {
+            labelBirthdayCost.Text = (birthdayParty.CalculateCost()).ToString("c");
+        }
+
+
+        #endregion
+
+
+        #region Dinner_Party
+
+        private void numericUpDownDinner_ValueChanged(object sender, EventArgs e)
+        {
+            recalculateDinnerEverything();
+            DisplayDinnerPartyCost();
+        }
+
+        private void checkBoxDinnerFancy_CheckedChanged(object sender, EventArgs e)
+        {
+            recalculateDinnerEverything();
+            DisplayDinnerPartyCost();
+        }
+
+        private void checkBoxDinnerHealthy_CheckedChanged(object sender, EventArgs e)
+        {
+            recalculateDinnerEverything();
+            DisplayDinnerPartyCost();
+        }
+
+        private void recalculateDinnerEverything()
+        {
+            dinnerParty.NumberOfPeople = (int)numericUpDownDinner.Value;
+            dinnerParty.HealthyOption = checkBoxDinnerHealthy.Checked;
+            dinnerParty.FancyDecorations = checkBoxDinnerFancy.Checked;
+        }
+
+        private void DisplayDinnerPartyCost()
+        {
+            labelDinnerCost.Text = (dinnerParty.CalculateCost()).ToString("c");
+        }
+
+        #endregion
+
+ 
+
+
+    }
+}
