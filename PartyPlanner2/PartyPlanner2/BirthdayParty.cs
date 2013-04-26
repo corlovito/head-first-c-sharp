06457d9e9ccf69b1242b111f38fe708f35d9c217
David Baker <dbaker3@jimbomail.com>
2013-04-26 19:43:35 -0400

    party planner stuff

---
+++ PartyPlanner2/PartyPlanner2/BirthdayParty.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PartyPlanner2
+{
+    class BirthdayParty
+    {
+        public const int CostOfFoodPerPerson = 25;
+
+        public decimal CostOfDecorations = 0;
+        private bool fancyDecorations;
+        public int CakeSize;
+
+        public BirthdayParty(int numberOfPeople, bool fancyDecoration, string cakeWriting)
+        {
+
+        }
+        
+    }
+}
---
+++ PartyPlanner2/PartyPlanner2/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace PartyPlanner2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelCost = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBoxHealthy = new System.Windows.Forms.CheckBox();
+            this.checkBoxFancy = new System.Windows.Forms.CheckBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(187, 186);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.labelCost);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.checkBoxHealthy);
+            this.tabPage1.Controls.Add(this.checkBoxFancy);
+            this.tabPage1.Controls.Add(this.numericUpDown1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(179, 160);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Dinner Party";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(276, 235);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Birthday Party";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Number of People";
+            // 
+            // labelCost
+            // 
+            this.labelCost.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelCost.Location = new System.Drawing.Point(75, 118);
+            this.labelCost.Name = "labelCost";
+            this.labelCost.Size = new System.Drawing.Size(70, 15);
+            this.labelCost.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(34, 119);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Cost";
+            // 
+            // checkBoxHealthy
+            // 
+            this.checkBoxHealthy.AutoSize = true;
+            this.checkBoxHealthy.Location = new System.Drawing.Point(37, 88);
+            this.checkBoxHealthy.Name = "checkBoxHealthy";
+            this.checkBoxHealthy.Size = new System.Drawing.Size(96, 17);
+            this.checkBoxHealthy.TabIndex = 8;
+            this.checkBoxHealthy.Text = "Healthy Option";
+            this.checkBoxHealthy.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxFancy
+            // 
+            this.checkBoxFancy.AutoSize = true;
+            this.checkBoxFancy.Location = new System.Drawing.Point(37, 65);
+            this.checkBoxFancy.Name = "checkBoxFancy";
+            this.checkBoxFancy.Size = new System.Drawing.Size(115, 17);
+            this.checkBoxFancy.TabIndex = 7;
+            this.checkBoxFancy.Text = "Fancy Decorations";
+            this.checkBoxFancy.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(37, 39);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(96, 20);
+            this.numericUpDown1.TabIndex = 6;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(187, 186);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Party Planner 2.0";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelCost;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBoxHealthy;
+        private System.Windows.Forms.CheckBox checkBoxFancy;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
