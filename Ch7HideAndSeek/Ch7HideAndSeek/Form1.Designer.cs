a5a0ca8a8a9c918029f61a835f43d52bf654e6bc
David Baker <dbaker3@stevemail.com>
2013-06-21 15:20:54 -0400

    more hide & seek

--- Ch7HideAndSeek/Ch7HideAndSeek/Form1.Designer.cs
+++ Ch7HideAndSeek/Ch7HideAndSeek/Form1.Designer.cs
@@ -32,6 +32,8 @@
             this.buttonGoHere = new System.Windows.Forms.Button();
             this.buttonGoThroughDoor = new System.Windows.Forms.Button();
             this.comboBoxExits = new System.Windows.Forms.ComboBox();
+            this.buttonCheck = new System.Windows.Forms.Button();
+            this.buttonHide = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBoxDescription
@@ -50,6 +52,7 @@
             this.buttonGoHere.TabIndex = 1;
             this.buttonGoHere.Text = "Go here:";
             this.buttonGoHere.UseVisualStyleBackColor = true;
+            this.buttonGoHere.Visible = false;
             this.buttonGoHere.Click += new System.EventHandler(this.buttonGoHere_Click);
             // 
             // buttonGoThroughDoor
@@ -60,6 +63,7 @@
             this.buttonGoThroughDoor.TabIndex = 2;
             this.buttonGoThroughDoor.Text = "Go through the door";
             this.buttonGoThroughDoor.UseVisualStyleBackColor = true;
+            this.buttonGoThroughDoor.Visible = false;
             this.buttonGoThroughDoor.Click += new System.EventHandler(this.buttonGoThroughDoor_Click);
             // 
             // comboBoxExits
@@ -70,12 +74,36 @@
             this.comboBoxExits.Name = "comboBoxExits";
             this.comboBoxExits.Size = new System.Drawing.Size(222, 21);
             this.comboBoxExits.TabIndex = 3;
+            this.comboBoxExits.Visible = false;
+            // 
+            // buttonCheck
+            // 
+            this.buttonCheck.Location = new System.Drawing.Point(12, 221);
+            this.buttonCheck.Name = "buttonCheck";
+            this.buttonCheck.Size = new System.Drawing.Size(312, 23);
+            this.buttonCheck.TabIndex = 4;
+            this.buttonCheck.Text = "Check";
+            this.buttonCheck.UseVisualStyleBackColor = true;
+            this.buttonCheck.Visible = false;
+            this.buttonCheck.Click += new System.EventHandler(this.buttonCheck_Click);
+            // 
+            // buttonHide
+            // 
+            this.buttonHide.Location = new System.Drawing.Point(12, 250);
+            this.buttonHide.Name = "buttonHide";
+            this.buttonHide.Size = new System.Drawing.Size(312, 23);
+            this.buttonHide.TabIndex = 5;
+            this.buttonHide.Text = "Hide!";
+            this.buttonHide.UseVisualStyleBackColor = true;
+            this.buttonHide.Click += new System.EventHandler(this.buttonHide_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(336, 230);
+            this.ClientSize = new System.Drawing.Size(336, 283);
+            this.Controls.Add(this.buttonHide);
+            this.Controls.Add(this.buttonCheck);
             this.Controls.Add(this.comboBoxExits);
             this.Controls.Add(this.buttonGoThroughDoor);
             this.Controls.Add(this.buttonGoHere);
@@ -96,6 +124,8 @@
         private System.Windows.Forms.Button buttonGoHere;
         private System.Windows.Forms.Button buttonGoThroughDoor;
         private System.Windows.Forms.ComboBox comboBoxExits;
+        private System.Windows.Forms.Button buttonCheck;
+        private System.Windows.Forms.Button buttonHide;
     }
 }
 
--- Ch7HideAndSeek/Ch7HideAndSeek/Form1.cs
+++ Ch7HideAndSeek/Ch7HideAndSeek/Form1.cs
@@ -10,52 +10,74 @@ using System.Windows.Forms;
 
 namespace Ch7HideAndSeek
 {
+    
     public partial class Form1 : Form
     {
+        int Moves;
+        Opponent opponent;
+        Location currentLocation;
+
+        RoomWithDoor livingRoom;
+        Room diningRoom;
+        RoomWithDoor kitchen;
+        Room stairway;
+        RoomWithHidingPlace upstairsHallway;
+        RoomWithHidingPlace bathroom;
+        RoomWithHidingPlace masterBedroom;
+        RoomWithHidingPlace secondBedroom;
+
+        OutsideWithDoor frontYard;
+        OutsideWithDoor backYard;
+        OutsideWithHidingPlace garden;
+        OutsideWithHidingPlace driveway;
+
         public Form1()
         {
             InitializeComponent();
             createObjects();
-            
+            opponent = new Opponent(frontYard);
+            ResetGame(false);
         }
 
-        private Location currentLocation { get; set; }
+        //private Location currentLocation { get; set; }
 
         private void moveToANewLocation(Location newLocation)
         {
+            Moves++;
             currentLocation = newLocation;
-            comboBoxExits.Items.Clear();
-            foreach (Location item in currentLocation.Exits)
-            {
-                comboBoxExits.Items.Add(item.Name);
-            }
-            comboBoxExits.SelectedIndex = 0;
+            redrawForm();
 
-            textBoxDescription.Text = currentLocation.Description;
+            //comboBoxExits.Items.Clear();
+            //foreach (Location item in currentLocation.Exits)
+            //{
+            //    comboBoxExits.Items.Add(item.Name);
+            //}
+            //comboBoxExits.SelectedIndex = 0;
 
-            if (currentLocation is IHasExteriorDoor)
-                buttonGoThroughDoor.Visible = true;
-            else
-                buttonGoThroughDoor.Visible = false;
+            //textBoxDescription.Text = currentLocation.Description;
+
+            //if (currentLocation is IHasExteriorDoor)
+            //    buttonGoThroughDoor.Visible = true;
+            //else
+            //    buttonGoThroughDoor.Visible = false;
 
 
         }
 
         private void createObjects()
         {
-            RoomWithDoor livingRoom = new RoomWithDoor("Living Room", "an antique carpet", "an oak door with a brass knob", "behind the coat rack");
-            RoomWithDoor kitchen = new RoomWithDoor("Kitchen", "a wood cook stove", "a screen door", "inside the refrigerator");
-            Room diningRoom = new Room("Dining Room", "a cherry dining table with high-back chairs");
-            OutsideWithDoor frontYard = new OutsideWithDoor("Front Yard", "an oak door with a brass knob", true);
-            OutsideWithDoor backYard = new OutsideWithDoor("Back Yard", "a screen door", false);
-            OutsideWithHidingPlace garden = new OutsideWithHidingPlace("Garden", false, "inside the shed");
-            Room stairway = new Room("Stairway", "a wooden banister");
-            RoomWithHidingPlace upstairsHallway = new RoomWithHidingPlace("Upstairs Hallway", "a picture of a dog", "inside the closet");
-            RoomWithHidingPlace masterBedroom = new RoomWithHidingPlace("Master Bedroom", "a large bed", "under the bed");
-            RoomWithHidingPlace secondBedroom = new RoomWithHidingPlace("Second Bedroom", "a small bed", "under the bed");
-            RoomWithHidingPlace bathroom = new RoomWithHidingPlace("Bathroom", "a sink and a smelly unflushed toilet", "in the shower");
-            OutsideWithHidingPlace driveway = new OutsideWithHidingPlace("Driveway", true, "inside the garage");
-
+            livingRoom = new RoomWithDoor("Living Room", "an antique carpet", "an oak door with a brass knob", "behind the coat rack");
+            kitchen = new RoomWithDoor("Kitchen", "a wood cook stove", "a screen door", "inside the refrigerator");
+            diningRoom = new Room("Dining Room", "a cherry dining table with high-back chairs");
+            frontYard = new OutsideWithDoor("Front Yard", "an oak door with a brass knob", true);
+            backYard = new OutsideWithDoor("Back Yard", "a screen door", false);
+            garden = new OutsideWithHidingPlace("Garden", false, "inside the shed");
+            stairway = new Room("Stairway", "a wooden banister");
+            upstairsHallway = new RoomWithHidingPlace("Upstairs Hallway", "a picture of a dog", "inside the closet");
+            masterBedroom = new RoomWithHidingPlace("Master Bedroom", "a large bed", "under the bed");
+            secondBedroom = new RoomWithHidingPlace("Second Bedroom", "a small bed", "under the bed");
+            bathroom = new RoomWithHidingPlace("Bathroom", "a sink and a smelly unflushed toilet", "in the shower");
+            driveway = new OutsideWithHidingPlace("Driveway", true, "inside the garage");
 
             // Set outside doors
             livingRoom.DoorLocation = frontYard;
@@ -78,7 +100,7 @@ namespace Ch7HideAndSeek
             driveway.Exits = new Location[] { frontYard, backYard };
 
 
-            moveToANewLocation(frontYard);  // start in front yard
+            //moveToANewLocation(frontYard);  // start in front yard
         }
 
         private void buttonGoHere_Click(object sender, EventArgs e)
@@ -90,6 +112,73 @@ namespace Ch7HideAndSeek
         {
             IHasExteriorDoor hasDoorLocation = currentLocation as IHasExteriorDoor;
             moveToANewLocation(hasDoorLocation.DoorLocation);   
+        }
+
+        private void ResetGame(bool displayMessage)
+        {
+            if (displayMessage)
+            {
+                MessageBox.Show("You found me in " + Moves + " moves!");
+                IHidingPlace foundLocation = currentLocation as IHidingPlace;
+                textBoxDescription.Text = "You found your opponent in " + Moves + " moves! He was hiding " + foundLocation.HidingPlace + ".";
+            }
+            Moves = 0;
+            buttonHide.Visible = true;
+            buttonGoHere.Visible = false;
+            buttonCheck.Visible = false;
+            buttonGoThroughDoor.Visible = false;
+            comboBoxExits.Visible = false;
+        }
+
+        private void buttonHide_Click(object sender, EventArgs e)
+        {
+            buttonHide.Visible = false;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                opponent.Move();
+                textBoxDescription.Text = i + "... ";
+                Application.DoEvents();
+                System.Threading.Thread.Sleep(200);
+            }
+
+            textBoxDescription.Text = "Ready or not, here I come!";
+            Application.DoEvents();
+            System.Threading.Thread.Sleep(500);
+
+            buttonGoHere.Visible = true;
+            comboBoxExits.Visible = true;
+            moveToANewLocation(livingRoom);
+        }
+
+        private void buttonCheck_Click(object sender, EventArgs e)
+        {
+            Moves++;
+            if (opponent.Check(currentLocation))
+                ResetGame(true);
+            else
+                redrawForm();
+        }
+
+        private void redrawForm()
+        {
+            comboBoxExits.Items.Clear();
+            for (int i = 0; i < currentLocation.Exits.Length; i++)
+                comboBoxExits.Items.Add(currentLocation.Exits[i].Name);
+            comboBoxExits.SelectedIndex = 0;
+            textBoxDescription.Text = currentLocation.Description + "\r\n\r\n(move #" + Moves + ")";
+            if (currentLocation is IHidingPlace)
+            {
+                IHidingPlace hidingPlace = currentLocation as IHidingPlace;
+                buttonCheck.Text = "Check " + hidingPlace.HidingPlace;
+                buttonCheck.Visible = true;
+            }
+            else
+                buttonCheck.Visible = false;
+            if (currentLocation is IHasExteriorDoor)
+                buttonGoThroughDoor.Visible = true;
+            else
+                buttonGoThroughDoor.Visible = false;
         }
 
     }
